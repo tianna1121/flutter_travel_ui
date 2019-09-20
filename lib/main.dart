7c69f964e59f0092594132ae4ad39f2a75fe6490
yang zhang <tianna1121@gmail.com>
2019-09-20 11:08:49 +0800

    add _buildCard(String title, String rating, String imgPath) function.

--- lib/main.dart
+++ lib/main.dart
@@ -123,9 +123,195 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Most Popular',
+                    style: TextStyle(fontFamily: 'Opensans', fontSize: 20.0),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Container(
+              height: 300.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  _buildCard('Pamir Mountains, China', '4.1',
+                      'assets/images/mountain.jpg'),
+                  _buildCard('Kathmandu city, Nepal', '3.8',
+                      'assets/images/kathmandu.jpg'),
+                ],
+              ),
+            ),
           )
         ],
       ),
     );
   }
+}
+
+Widget _buildCard(String title, String rating, String imgPath) {
+  return Padding(
+    padding: EdgeInsets.all(10.0),
+    child: InkWell(
+      onTap: () {},
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 275.0,
+            width: 200.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              image: DecorationImage(
+                  image: AssetImage(imgPath), fit: BoxFit.cover),
+            ),
+          ),
+          // * make the shade a bit deeper
+          Container(
+            height: 275.0,
+            width: 200.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.black.withOpacity(0.4),
+            ),
+          ),
+          Positioned(
+            top: 10.0,
+            left: 10.0,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  height: 40.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.black.withOpacity(0.2),
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.star,
+                          color: Colors.white,
+                          size: 12.0,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          rating,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 50.0,
+                ),
+                Text(
+                  'More',
+                  style: TextStyle(color: Colors.white, fontFamily: 'Opensans'),
+                ),
+                SizedBox(
+                  width: 7.0,
+                ),
+
+                // * This should be a IconButton in real app.
+                Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.white,
+                  size: 25.0,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 165.0,
+            left: 10.0,
+            child: Container(
+              width: 150.0,
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontFamily: 'Opensans',
+                    fontSize: 17.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 225.0,
+            left: 10.0,
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'I was here',
+                  style: TextStyle(
+                      fontFamily: 'Opensans',
+                      fontSize: 15.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 40.0,
+                      width: 100.0,
+                    ),
+                    Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/profilepic.jpg'),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Positioned(
+                      left: 30.0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.white),
+                        child: Center(
+                          child: Text(
+                            '+17 ..',
+                            style:
+                                TextStyle(fontSize: 14.0, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
